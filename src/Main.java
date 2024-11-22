893bd5e4e5b9f9c96011060a881851b97baf723b
sierrapablo <pablosierralorente@outlook.es>
2024-11-22 01:09:22 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@
 import models.Juego;
 
 public class Main {
+
     static String jugador1Nombre = "Paco";
     static int numCarasDado1 = 6;
+
     static String jugador2Nombre = "Pepe";
     static int numCarasDado2 = 6;
-    static int numTiradas = 2000;
+
+    static int numTiradas = 10;
 
     public static void main(String[] args) {
         Juego partidaDados = new Juego(
